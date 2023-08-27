6554321406c960b684f0d6837ad7819f95e2f69c
João Alonso <joaoaalonso@gmail.com>
2023-08-27 18:04:09 -0300

    adding button to copy code

--- lib/shared/widgets/markdown/code_render.dart
+++ lib/shared/widgets/markdown/code_render.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_highlight/themes/atom-one-dark.dart';
 import 'package:flutter_html/flutter_html.dart';
@@ -11,26 +12,47 @@ class CodeRender extends StatelessWidget {
 
   const CodeRender(this.renderContext, this.buildChild, {super.key});
 
+  void copyToClipboard(BuildContext context, String content) {
+    Clipboard.setData(ClipboardData(text: content));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Código copiado para a área de transferência.'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final rawContent = renderContext.tree.children;
     final rawString = rawContent[0].toString();
     final content = _sanitize(rawString.substring(1, rawString.length - 1));
-    print(content);
     final result = highlight.parse(content, autoDetection: true);
 
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: HighlightView(
-          content.replaceAll('\\n', '\n'),
-          language: result.language ?? 'dart',
-          padding: const EdgeInsets.all(16),
-          theme: atomOneDarkTheme,
+    return Stack(children: [
+      SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: HighlightView(
+            content.replaceAll('\\n', '\n'),
+            language: result.language ?? 'dart',
+            padding: const EdgeInsets.all(16),
+            theme: atomOneDarkTheme,
+          ),
         ),
       ),
-    );
+      Align(
+        alignment: AlignmentDirectional.topEnd,
+        child: IconButton(
+          onPressed: () => copyToClipboard(context, content),
+          icon: const Icon(
+            Icons.copy,
+            color: Colors.white,
+            size: 14,
+          ),
+        ),
+      ),
+    ]);
   }
 
   String _sanitize(String rawContent) {
